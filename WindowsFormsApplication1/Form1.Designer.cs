925f687742dc0cfa7c91395ff7d641e6a133307a
OST <ost5253@gmail.com>
2016-09-07 13:47:32 +0900

    commit

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -30,12 +30,16 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Cursor = System.Windows.Forms.Cursors.No;
-            this.button1.Location = new System.Drawing.Point(389, 467);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(206, 469);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,17 +50,56 @@
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(39, 86);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(221, 245);
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(467, 327);
             this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(527, 210);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(82, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(255, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(398, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(853, 639);
+            this.ClientSize = new System.Drawing.Size(696, 639);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -69,6 +112,10 @@
         #endregion
         public System.Windows.Forms.Button button1;
         public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,7 +3,9 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.Specialized;
 using System.Data;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Windows.Forms;
@@ -12,39 +14,108 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        
+        sql_connection_manager conn_manager;
+
+        int i = 0;
 
         public Form1()
         {
             InitializeComponent();
 
+            conn_manager = new sql_connection_manager(this);
+
+            DataRow r = conn_manager.version_control();
+
+            textBox1.Text = r["LatestVersion"].ToString();
+
+            conn_manager.like_up();
+
         }
 
         public void like_proc()
         {
-            insta_procedure insta_run = new insta_procedure(this);
+            try
+            {
 
-            insta_run.start();
-            insta_run.login();
-            insta_run.go_to_there("/yunakim/");
+                insta_procedure insta_run = new insta_procedure(this, conn_manager);
 
-            //insta_run.follow_account();
-            insta_run.like_loop();
-            //insta_run.logout();
+                //시작
+                
+
+                while (true)
+                {
+
+                    insta_run.start(i++);
+
+                    //로그인
+                    insta_run.login();
+                    //1. 해시태그 검색
+                    insta_run.hash_tag_search();
+
+                    
+                    //좋아요 + 팔로우 루프
+                    insta_run.like_follow_loop();
+
+                    
+
+                    //2. 등록된 유저 검색
+                    insta_run.random_user();
+                    //좋아요 루프
+                    insta_run.like_loop();
+
+                    //insta_run.logout();
+
+                    //3. 요청 유저
+                    //팔로우, 좋아요
+
+                    insta_run.quit();
+
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.StackTrace;
+            }
         }
 
-        
+
 
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             Thread like_thr = new Thread(like_proc);
             like_thr.Start();
 
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["mb_id"] = textBox2.Text;
+                values["mb_password"] = textBox3.Text;
+                values["easygram"] = "K";
 
+                var response = client.UploadValues("http://www.easygram.kr/manager/bbs/login_check.php", values);
 
-
+                var responseString = Encoding.Default.GetString(response);
+                
+                if(responseString == "clear")
+                {
+                    MessageBox.Show("success");
+                    textBox1.AppendText("로그인 성공");
+                }
+                else
+                {
+                    MessageBox.Show("계정 정보를 다시 확인 하세요");
+                    textBox1.AppendText("로그인 실패");
+                }
+            }
+        }
     }
 }
