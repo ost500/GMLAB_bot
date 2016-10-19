132d078712c50d188ba12b88b1eb3995b5e7e2e6
OST <ost5253@gmail.com>
2016-10-19 09:49:30 +0900

    conn close when mysql connection refresh

--- WindowsFormsApplication1/connection_manager.cs
+++ WindowsFormsApplication1/connection_manager.cs
@@ -456,7 +456,8 @@ namespace WindowsFormsApplication1
         {
             try
             {
-                
+                conn.Close();
+
                 String strConn = "Server=110.35.167.2;Database=easygram;Uid=easygram;Pwd=tU2LHxyyTppHUGvw;";
                 conn = new MySqlConnection(strConn);
 
